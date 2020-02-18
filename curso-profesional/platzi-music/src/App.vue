f9002fc520390853d4828cfdfb3059504add684f
jorge <jlcallalle@gmail.com>
2020-02-18 14:04:16 -0500

    se agrega vue table

--- curso-profesional/platzi-music/src/App.vue
+++ curso-profesional/platzi-music/src/App.vue
@@ -13,11 +13,22 @@
     <p v-if="showValue"> {{ value }}</p>
     <p v-else> {{ 'algo más' }}</p>
     
-    <ul>
-    <li v-for="item in items" :key="item">
-      {{ item.mensaje }}
-    </li>
-    </ul>
+    <!-- <ul>
+      <li v-for="item in items" :key="item">
+        {{ item.mensaje }}
+      </li>
+    </ul> -->
+
+    <br>
+    <input ref="entrada"></input>
+    <input ref="entrada2"></input>
+
+    <h1>{{ message }}</h1>
+  	<button @click="clickedButton">Click Me!</button>
+
+
+     <v-client-table :data="tableData" :columns="columns"/>
+
 
   </div>
 </template>
@@ -27,6 +38,7 @@ export default {
   name: 'app',
   data () {
     return {
+      message: 'Hello World!',
       msg: 'Hola Vue.js',
       person: {
           name: 'juan'
@@ -34,12 +46,32 @@ export default {
       mostrarContenido: false,
       showValue: false,
       value : 'algo',
-       items: [
+      items: [
         { mensaje: 'Foo' },
         { mensaje: 'Bar' }
-      ]
+      ],
+      columns: ['id', 'name', 'age'],
+      tableData: [
+          { id: 1, name: "John", age: "20" },
+          { id: 2, name: "Jane", age: "24" },
+          { id: 3, name: "Susan", age: "16" },
+          { id: 4, name: "Chris", age: "55" },
+          { id: 5, name: "Dan", age: "40" },
+      ],
+      options: {
+          // see the options API
+      }
     }
-  }
+  },
+  methods: {
+    clickedButton: function() {
+			console.log(this.$refs, 'evento click button');
+		}
+  },
+  mounted()
+	{
+		console.log( this.$refs , 'mounted load $refs ');
+	}
 }
 </script>
 
--- curso-profesional/platzi-music/src/main.js
+++ curso-profesional/platzi-music/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {ServerTable, ClientTable, Event} from 'vue-tables-2';
+
+Vue.use(ClientTable, {}, false, 'bootstrap3')
+
+
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+console.log('hola')
